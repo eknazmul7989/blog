001a5d2dd668d85bc22dc101b74a951b384534c4
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2021-12-11 07:46:14 +0600

    Add Home component
    Add new component named Home component

---
+++ my-app/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+             <h3 style={{'text-align': "center",'font-size': '50px'}}>Home component </h3>
+            </>
+        );
+    }
+}
+
+export default Home;
